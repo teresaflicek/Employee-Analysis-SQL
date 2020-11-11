f7fbdd79b6414e03a8975798d03482f83bb55025
teresaflicek <69260261+teresaflicek@users.noreply.github.com>
2020-11-11 14:16:04 -0600

    data analysis complete

--- Data_Analysis.sql
+++ Data_Analysis.sql
@@ -1,2 +1,86 @@
 --Data Analysis
 
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+select employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+from employees
+left join salaries
+on employees.emp_no=salaries.emp_no
+order by emp_no;
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+select employees.first_name,
+	employees.last_name,
+	employees.hire_date
+from employees 
+where hire_date between '1986-01-01' and '1986-12-31';
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+select departments.dept_no, 
+	departments.dept_name, 
+	dept_manager.emp_no, 
+	employees.last_name,
+	employees.first_name
+from dept_manager
+join departments
+	on departments.dept_no=dept_manager.dept_no
+join employees
+	on employees.emp_no=dept_manager.emp_no
+order by dept_no;
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+select employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+from employees
+join dept_employees
+	on employees.emp_no=dept_employees.emp_no
+join departments
+	on departments.dept_no=dept_employees.dept_no
+order by dept_name;
+	
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select employees.first_name,
+	employees.last_name,
+	employees.sex
+from employees
+where first_name = 'Hercules'
+and last_name like 'B%'
+order by last_name;
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+from employees
+join dept_employees
+	on employees.emp_no=dept_employees.emp_no
+join departments
+	on departments.dept_no=dept_employees.dept_no
+where departments.dept_name = 'Sales'
+order by last_name;
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+from employees
+join dept_employees
+	on employees.emp_no=dept_employees.emp_no
+join departments
+	on departments.dept_no=dept_employees.dept_no
+where departments.dept_name in ('Sales', 'Development')
+order by last_name;
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count(last_name) as frequency
+from employees
+group by last_name
+order by count(last_name) desc;
--- Table_Schemata.sql
+++ Table_Schemata.sql
@@ -1,31 +1,31 @@
 --Data Engineering
 
-CREATE TABLE departments(
+create table departments(
 	dept_no VARCHAR NOT NULL PRIMARY KEY,
 	dept_name VARCHAR);
 	
-SELECT * FROM departments;
+select * from departments;
 
 create table dept_employees(
 	emp_no INT,
 	dept_no VARCHAR);
 	
-SELECT * FROM dept_employees;
+select * from dept_employees;
 
 create table dept_manager(
 	dept_no VARCHAR,
 	emp_no INT);
 	
-SELECT * FROM dept_manager;
+select * from dept_manager;
 
 create table employees(
 	emp_no INT NOT NULL PRIMARY KEY,
 	emp_title_id VARCHAR,
-	birth_date VARCHAR,
+	birth_date DATE,
 	first_name VARCHAR,
 	last_name VARCHAR,
 	sex VARCHAR,
-	hire_date VARCHAR);
+	hire_date DATE);
 	
 select * from employees;
 
